007c4d696b17903a732a225693e97820b615cacc
Nithishclin <nithishclin012@gmail.com>
2023-12-25 11:08:36 +0530

    Quizz game

--- python.py
+++ python.py
@@ -1,2 +1,42 @@
-for x in range (0,200,5):
-    print(x)
+print("Welcome to the computer Quizz")
+
+player = input("Do you want to play this Game? ")
+
+if player != "yes":
+    quit()
+
+print("Lets start the game :) ")
+
+score = 0
+
+answer = input("What does the CPU stands for? ")
+if answer == "central processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What does the GPU stands for? ")
+if answer == "graphics processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What does the RAM stands for? ")
+if answer == "random access memory":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+answer = input("What does the PSU stands for? ")
+if answer == "power supply unit":
+    print("correct!")
+    score += 1
+else:
+    print("incorrect!")
+
+print("you got "+ str(score)+ " question correct!")
+
+print("you got "+ str((score / 4) * 100)+ "%.")
