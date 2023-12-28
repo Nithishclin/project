a6b276a36f1e9ab83021b6c9aac5333cc028d40d
Nithishclin <nithishclin012@gmail.com>
2023-12-28 14:59:47 +0530

    Clock

--- clock.py
+++ clock.py
@@ -1,1 +1,36 @@
 
+import tkinter
+from tkinter import *
+
+
+# importing strftime function to capturing system's time
+
+from time import strftime
+
+# creating tkinter window
+digital_clock = Tk()
+digital_clock.geometry("300x100")
+digital_clock.title('Digital Clock')
+
+
+# Display time on the label using this function
+def time():
+    string = strftime('%H:%M:%S %p')  # %h=hour ,%M=Minute ,%S=second ,p%=am or pm according to the given time value
+    lable.config(text = string)
+    lable.after(1000, time)
+
+
+#Increase the user experience designning the label widget
+
+lable = Label(digital_clock, font = ('calibre', 40, 'bold'),
+            background = 'black',
+            foreground = 'white')
+
+
+# clock at the centre of the tkinter window
+
+lable.pack(anchor = 'center')
+time()  #function calling
+
+mainloop()
+
